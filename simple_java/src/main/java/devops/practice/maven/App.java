7f87236b24dc2c7d8f7b5d69954baa32205ab4e0
satyadevops <34495025+satyadevops@users.noreply.github.com>
2018-01-17 15:15:54 -0800

    Update App.java
    commite

--- simple_java/src/main/java/devops/practice/maven/App.java
+++ simple_java/src/main/java/devops/practice/maven/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World
-                           
-1st commit!" );
+        System.out.println( "Hello World 1st commit!" );
     }
 }
